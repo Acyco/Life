b4d933597de974b540ae51ae4a03091f5603b5ea
Acyco <769225491@qq.com>
2020-03-26 07:23:22 +0800

    Initial commit

---
+++ src/main/java/cn/acyco/life/Life.java
@@ -0,0 +1,11 @@
+package cn.acyco.life;
+
+/**
+ * @author Acyco
+ * @create 2020-03-26 07:02
+ */
+public class Life {
+    public static void main(String[] args){
+        System.out.println("Acyco Life");
+    }
+}
